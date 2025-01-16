16364af97e3775c60c37e8bb45572a709b348cc9
r-snyder <ryanjsnyder@proton.me>
2025-01-16 12:19:12 -0400

    Update sample.c
    fixed codeql issues

--- sample.c
+++ sample.c
@@ -1,17 +1,32 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
-#define BUFSIZE 256
-    
-// This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
+
+int main(int argc, char **argv) {
     // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
-        return -1;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    // Open the file in read mode
+    FILE *file = fopen(argv[1], "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Unable to open file '%s'.\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    // Calculate the file size
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fclose(file);
+
+    if (file_size < 0) {
+        fprintf(stderr, "Error: Failed to determine the size of '%s'.\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    // Print the file size
+    printf("The size of '%s' is %ld bytes.\n", argv[1], file_size);
+
+    return EXIT_SUCCESS;
 }
